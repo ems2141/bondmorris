fd730cb8f20a82459d65d456f45fc5f6282afd51
Ellie Schneiders <ems2141@gmail.com>
2014-10-27 13:05:00 -0600

    WIP commit

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'foundation-rails'
+gem 'rails_12factor', group: :production
 
 group :test, :development do
   gem 'rspec-rails', '~> 3.1.0'
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,6 +4,14 @@ describe 'homepage' do
   it 'has a welcome page' do
     visit '/'
 
-    expect(page).to have_content 'Bond and Morris'
+    expect(page).to have_content 'Bond & Morris'
+
+    click_on 'Contact'
+
+    expect(page).to have_content 'Denver'
+    expect(page).to have_content '303 E. 17th Avenue, Suite 888'
+    expect(page).to have_content 'Denver, Colorado 80203'
+    expect(page).to have_content 'Phone: (303) 837-9222'
+    expect(page).to have_content 'Fax: (303) 837-0849'
   end
 end
